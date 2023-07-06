b18834ae684271d0338eb7b2d5a4376d93c909fc
Adamantios <adamantios.zaras@valory.xyz>
2023-07-06 21:40:37 +0300

    refactor: move behaviours to package

--- packages/valory/skills/decision_maker_abci/behaviours/decision_maker.py
+++ packages/valory/skills/decision_maker_abci/behaviours/decision_maker.py
@@ -1,39 +1,22 @@
-# -*- coding: utf-8 -*-
-# ------------------------------------------------------------------------------
-#
-#   Copyright 2023 Valory AG
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
-# ------------------------------------------------------------------------------
-
-"""This module contains the behaviours for the 'decision_maker_abci' skill."""
-
 from multiprocessing.pool import AsyncResult
-from typing import Set, Type, cast, Any, Optional, Generator, Tuple
+from string import Template
+from typing import Any, Optional, cast, Generator, Tuple
 
 from packages.valory.skills.abstract_round_abci.base import BaseTxPayload
-from packages.valory.skills.abstract_round_abci.behaviours import (
-    AbstractRoundBehaviour,
-    BaseBehaviour,
-)
+from packages.valory.skills.abstract_round_abci.behaviour_utils import BaseBehaviour
 from packages.valory.skills.decision_maker_abci.models import DecisionMakerParams
 from packages.valory.skills.decision_maker_abci.payloads import DecisionMakerPayload
-from packages.valory.skills.decision_maker_abci.rounds import (
-    AgentDecisionMakerAbciApp,
-    DecisionMakerRound, SynchronizedData
-)
+from packages.valory.skills.decision_maker_abci.rounds import DecisionMakerRound, SynchronizedData
 from packages.valory.skills.decision_maker_abci.tasks import MechInteractionTask, MechInteractionResponse
+
+BET_PROMPT = Template(
+    """
+    With the given question "${question}" 
+    and the `yes` option represented by ${yes} 
+    and the `no` option represented by ${no}, 
+    what are the respective probabilities of `p_yes` and `p_no` occurring?
+    """
+)
 
 
 class DecisionMakerBehaviour(BaseBehaviour):
@@ -59,21 +42,20 @@ class DecisionMakerBehaviour(BaseBehaviour):
     def setup(self) -> None:
         """Setup behaviour."""
         mech_task = MechInteractionTask()
-        sampled_bet = self.synchronized_data.sampled_bet.title
+        sampled_bet = self.synchronized_data.sampled_bet
+        prompt_params = dict(
+            question=sampled_bet.title, yes=sampled_bet.yes, no=sampled_bet.no
+        )
         task_kwargs = dict(
-            question=sampled_bet.title,
-            yes=sampled_bet.yes,
-            no=sampled_bet.no,
+            prompt=BET_PROMPT.substitute(prompt_params),
             agent_id=self.params.mech_agent_id,
             tool=self.params.mech_tool,
-            private_key_path=self.context.private_key_paths.read("ethereum")
+            private_key_path=self.context.private_key_paths.read("ethereum"),
         )
-        task_id = self.context.task_manager.enqueue_task(
-            mech_task, kwargs=task_kwargs
-        )
+        task_id = self.context.task_manager.enqueue_task(mech_task, kwargs=task_kwargs)
         self._async_result = self.context.task_manager.get_task_result(task_id)
 
-    def _get_decision(self) -> Generator[None, None, Optional[Tuple[bool, float]]]:
+    def _get_decision(self) -> Generator[None, None, Optional[Tuple[Optional[int], float]]]:
         """Get the vote and it's confidence."""
         self._async_result = cast(AsyncResult, self._async_result)
         if not self._async_result.ready():
@@ -83,12 +65,11 @@ class DecisionMakerBehaviour(BaseBehaviour):
 
         # Get the decision from the task.
         mech_response = cast(MechInteractionResponse, self._async_result.get())
-        self.context.logger.info(
-            f"Decision has been received:\n{mech_response}"
-        )
+        self.context.logger.info(f"Decision has been received:\n{mech_response}")
 
         if mech_response.prediction is None:
-            return False, 0
+            self.context.logger.info(f"There was an error on the mech response: {mech_response.error}")
+            return None, -1
 
         return mech_response.prediction.vote, mech_response.prediction.confidence
 
@@ -112,13 +93,3 @@ class DecisionMakerBehaviour(BaseBehaviour):
             payload = DecisionMakerPayload(self.context.agent_address, vote, confidence)
 
         yield from self.finish_behaviour(payload)
-
-
-class AgentDecisionMakerRoundBehaviour(AbstractRoundBehaviour):
-    """This behaviour manages the consensus stages for the decision making."""
-
-    initial_behaviour_cls = DecisionMakerBehaviour
-    abci_app_cls = AgentDecisionMakerAbciApp
-    behaviours: Set[Type[BaseBehaviour]] = {
-        DecisionMakerBehaviour,  # type: ignore
-    }
