e1aa3036fd54e3ca3e2a81463ca38e643d2ecca3
Adamantios <adamantios.zaras@valory.xyz>
2023-07-07 10:22:36 +0300

    fix: avoid `StopIteration` and return `None` as a default

--- packages/valory/skills/market_manager_abci/graph_tooling/requests.py
+++ packages/valory/skills/market_manager_abci/graph_tooling/requests.py
@@ -94,7 +94,7 @@ class QueryingBehaviour(BaseBehaviour, ABC):
     def _prepare_bets_fetching(self) -> bool:
         """Prepare for fetching a bet."""
         if self._fetch_status in (FetchStatus.SUCCESS, FetchStatus.NONE):
-            res = next(self._creators_iterator)
+            res = next(self._creators_iterator, None)
             if res is None:
                 return False
             self._current_market, self._current_creators = next(self._creators_iterator)
