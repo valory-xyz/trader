2309b1c29d358ab67afd4e4a14657444ed4db6e7
Jannik Hehemann <jannik@hehemann-melle.de>
2023-10-20 16:17:52 +0200

    chore: Convert filter to list

--- packages/valory/skills/decision_maker_abci/behaviours/sampling.py
+++ packages/valory/skills/decision_maker_abci/behaviours/sampling.py
@@ -59,7 +59,7 @@ class SamplingBehaviour(DecisionMakerBaseBehaviour):
         # Get only bets that close in the next 48 hours
         # Note: the openingTimestamp is misleading as it is the closing timestamp of the bet
         short_term_bets = filter(lambda bet: bet.openingTimestamp <= (time.time() + 172800), bets)
-        self.context.logger.info(f"Short term bets: {short_term_bets}")
+        self.context.logger.info(f"Short term bets: {list(short_term_bets)}")
         return self.synchronized_data.bets.index(max(short_term_bets))
 
     def _set_processed(self, idx: int) -> Optional[str]:
