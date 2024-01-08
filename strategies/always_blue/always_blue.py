9cfd7ce332f30996b7c35dbaa2d02f5d3f5d9ce1
Robin Lehmann <4robinlehmann@gmail.com>
2024-01-08 22:55:47 +0700

    removed unused code

--- strategies/always_blue/always_blue.py
+++ strategies/always_blue/always_blue.py
@@ -22,41 +22,16 @@
 from typing import Dict, Any, List, Union
 
 REQUIRED_FIELDS = frozenset()
-OPTIONAL_FIELDS = frozenset({"max_bet"})
-ALL_FIELDS = REQUIRED_FIELDS.union(OPTIONAL_FIELDS)
-DEFAULT_MAX_BET = 8e17
+ALL_FIELDS = REQUIRED_FIELDS
 
 
-def check_missing_fields(kwargs: Dict[str, Any]) -> List[str]:
-    """Check for missing fields and return them, if any."""
-    missing = []
-    for field in REQUIRED_FIELDS:
-        if kwargs.get(field, None) is None:
-            missing.append(field)
-    return missing
-
-
-def remove_irrelevant_fields(kwargs: Dict[str, Any]) -> Dict[str, Any]:
-    """Remove the irrelevant fields from the given kwargs."""
-    return {key: value for key, value in kwargs.items() if key in ALL_FIELDS}
-
-
-def wei_to_native(wei: int) -> float:
-    """Convert WEI to native token."""
-    return wei / 10**18
-
-
-def get_always_blue(  # pylint: disable=too-many-arguments
-) -> Dict[str, Union[int, List[str]]]:
+def get_always_blue() -> Dict[str, Union[int, str]]:
     """ALWAYS BLUE."""
-    return {"bet_amount": 0, "info": "ALWAYS BLUE!", "error": "IT WAS NOT BLUE!"}
+    return {"bet_amount": 0, "info": "ALWAYS BLUE!"}
 
 
 def run(*_args, **kwargs) -> Dict[str, Union[int, List[str]]]:
     """Run the strategy."""
-    missing = check_missing_fields(kwargs)
-    if len(missing) > 0:
-        return {"error": [f"Required kwargs {missing} were not provided."]}
 
     kwargs = remove_irrelevant_fields(kwargs)
-    return get_always_blue(**kwargs)
+    return get_always_blue()
