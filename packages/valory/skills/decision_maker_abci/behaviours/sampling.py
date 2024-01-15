98108d99442b7ee37b4bd93526540860787e84e7
Adamantios <adamantios.zaras@valory.xyz>
2024-01-15 12:25:34 +0200

    fix: if no bet was sampled, emit a `NONE` event

--- packages/valory/skills/decision_maker_abci/behaviours/sampling.py
+++ packages/valory/skills/decision_maker_abci/behaviours/sampling.py
@@ -96,7 +96,10 @@ class SamplingBehaviour(DecisionMakerBaseBehaviour):
             self.read_bets()
             idx = self._sample()
             self.store_bets()
-            bets_hash = self.hash_stored_bets()
+            if idx is None:
+                bets_hash = None
+            else:
+                bets_hash = self.hash_stored_bets()
             payload = SamplingPayload(self.context.agent_address, bets_hash, idx)
 
         yield from self.finish_behaviour(payload)
