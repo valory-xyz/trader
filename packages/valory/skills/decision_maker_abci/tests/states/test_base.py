9c1f1f617cddf7a4a8f2f93b162ebc1f63c19938
Ravleen-Solulab <ravleen@solulab.co>
2024-09-20 18:42:13 +0530

    tests:state

--- packages/valory/skills/decision_maker_abci/tests/states/test_base.py
+++ packages/valory/skills/decision_maker_abci/tests/states/test_base.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 #
-#   Copyright 2023 Valory AG
+#   Copyright 2023-2024 Valory AG
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 #   limitations under the License.
 #
 # ------------------------------------------------------------------------------
+
 import json
 from unittest.mock import MagicMock, patch
 from typing import Generator, Callable, List, Mapping, Any, Tuple
--- packages/valory/skills/decision_maker_abci/tests/states/test_bet_placement.py
+++ packages/valory/skills/decision_maker_abci/tests/states/test_bet_placement.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 #
-#   Copyright 2023 Valory AG
+#   Copyright 2023-2024 Valory AG
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 #   limitations under the License.
 #
 # ------------------------------------------------------------------------------
+
 import pytest
 from packages.valory.skills.decision_maker_abci.states.bet_placement import BetPlacementRound
 from packages.valory.skills.decision_maker_abci.states.base import Event
