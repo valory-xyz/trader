ec8d89b92feca7ea6441c08f31366ef5133dd040
Anna Sambrook <annasambrook@gmail.com>
2025-03-24 10:16:58 +0000

    feat: improve logging for read stored timestamp error.

--- packages/valory/skills/staking_abci/behaviours.py
+++ packages/valory/skills/staking_abci/behaviours.py
@@ -427,8 +427,12 @@ class CallCheckpointBehaviour(
                     return int(checkpoint_file.readline())
                 except (ValueError, TypeError, StopIteration):
                     err = f"Stored checkpoint timestamp could not be parsed from {self._checkpoint_filepath!r}!"
-        except (FileNotFoundError, PermissionError, OSError):
-            err = f"Error opening file {self._checkpoint_filepath!r} in {READ_MODE!r} mode!"
+        except FileNotFoundError:
+            err = f"Error opening file {self._checkpoint_filepath!r} in {READ_MODE!r} mode! File not found!"
+        except PermissionError:
+            err = f"Error opening file {self._checkpoint_filepath!r} in {READ_MODE!r} mode! Permission denied!"
+        except OSError:
+            err = f"Error opening file {self._checkpoint_filepath!r} in {READ_MODE!r} mode! OS error!"
 
         self.context.logger.error(err)
         return None
