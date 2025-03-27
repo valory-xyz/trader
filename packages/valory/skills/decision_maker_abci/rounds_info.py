a5adfeb416a657be5441e2cb2045a6bfdf155416
Anna Sambrook <annasambrook@gmail.com>
2025-03-27 18:37:08 +0000

    chore: generators

--- packages/valory/skills/decision_maker_abci/rounds_info.py
+++ packages/valory/skills/decision_maker_abci/rounds_info.py
@@ -1,5 +1,211 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
+#
+#   Copyright 2021-2025 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+"""This module contains the rounds info for the 'decision_maker_abci' skill."""
+
+ROUNDS_INFO = {
+    "benchmarking_randomness_round": {
+        "name": "Benchmarking Randomness Round",
+        "description": "Gathers randomness in benchmarking mode",
+        "transitions": {},
+    },
+    "bet_placement_round": {
+        "name": "Bet Placement Round",
+        "description": "Attempting to place a bet on a market.",
+        "transitions": {},
+    },
+    "blacklisting_round": {
+        "name": "Blacklisting Round",
+        "description": "Blacklisting the sampled bet and updates the bets.",
+        "transitions": {},
+    },
+    "call_checkpoint_round": {
+        "name": "Call Checkpoint Round",
+        "description": "Preparing to call the checkpoint",
+        "transitions": {},
+    },
+    "check_benchmarking_mode_round": {
+        "name": "Check Benchmarking Mode Round",
+        "description": "Checking if the benchmarking mode is enabled.",
+        "transitions": {},
+    },
+    "check_late_tx_hashes_round": {
+        "name": "Check Late Tx Hashes Round",
+        "description": "Checks the late transaction hashes to see if any of them have been validated",
+        "transitions": {},
+    },
+    "check_stop_trading_round": {
+        "name": "Check Stop Trading Round",
+        "description": "Checking if the conditions are met to stop trading",
+        "transitions": {},
+    },
+    "check_transaction_history_round": {
+        "name": "Check Transaction History Round",
+        "description": "Checks the transaction history to determine if any previous transactions have been validated",
+        "transitions": {},
+    },
+    "claim_round": {
+        "name": "Claim Round",
+        "description": "Prepares a claim transaction for the subscription the agent has purchased",
+        "transitions": {},
+    },
+    "collect_signature_round": {
+        "name": "Collect Signature Round",
+        "description": "Signs a transaction",
+        "transitions": {},
+    },
+    "decision_receive_round": {
+        "name": "Decision Receive Round",
+        "description": "Decides on the bet's answer based on mech response.",
+        "transitions": {},
+    },
+    "decision_request_round": {
+        "name": "Decision Request Round",
+        "description": "Prepares a mech request transaction to determine the answer to a bet",
+        "transitions": {},
+    },
+    "failed_multiplexer_round": {
+        "name": "Failed Multiplexer Round",
+        "description": "Represents a failure in identifying the transmitter round",
+        "transitions": {},
+    },
+    "finalization_round": {
+        "name": "Finalization Round",
+        "description": "Represents that the transaction signing has finished",
+        "transitions": {},
+    },
+    "handle_failed_tx_round": {
+        "name": "Handle Failed Tx Round",
+        "description": "Handles a failed transaction",
+        "transitions": {},
+    },
+    "impossible_round": {
+        "name": "Impossible Round",
+        "description": "Represents that it is impossible to reach a decision with the given parametrization",
+        "transitions": {},
+    },
+    "mech_request_round": {
+        "name": "Mech Request Round",
+        "description": "Preforms a mech request to determine the answer of a bet",
+        "transitions": {},
+    },
+    "mech_response_round": {
+        "name": "Mech Response Round",
+        "description": "Collects the responses from a Mech to determine the answer of a bet",
+        "transitions": {},
+    },
+    "post_tx_settlement_round": {
+        "name": "Post Tx Settlement Round",
+        "description": "Finished the transaction settlement",
+        "transitions": {},
+    },
+    "pre_tx_settlement_round": {
+        "name": "Pre Tx Settlement Round",
+        "description": "Ensures the pre transaction settlement checks have passed",
+        "transitions": {},
+    },
+    "randomness_round": {
+        "name": "Randomness Round",
+        "description": "Gathers randomness",
+        "transitions": {},
+    },
+    "randomness_transaction_submission_round": {
+        "name": "Randomness Transaction Submission Round",
+        "description": "Generates randomness",
+        "transitions": {},
+    },
+    "redeem_round": {
+        "name": "Redeem Round",
+        "description": "Prepares a transaction to redeem the winnings",
+        "transitions": {},
+    },
+    "registration_round": {
+        "name": "Registration Round",
+        "description": "Registers the agents. Waits until the threshold is reached",
+        "transitions": {},
+    },
+    "registration_startup_round": {
+        "name": "Registration Startup Round",
+        "description": "Registers the agents. Waits until all agents have registered",
+        "transitions": {},
+    },
+    "reset_and_pause_round": {
+        "name": "Reset And Pause Round",
+        "description": "Cleans up and sleeps for some time before running again",
+        "transitions": {},
+    },
+    "reset_round": {
+        "name": "Reset Round",
+        "description": "Cleans up and resets the agent",
+        "transitions": {},
+    },
+    "sampling_round": {
+        "name": "Sampling Round",
+        "description": "Samples a bet",
+        "transitions": {},
+    },
+    "select_keeper_transaction_submission_a_round": {
+        "name": "Select Keeper Transaction Submission A Round",
+        "description": "Selects a keeper for the transaction submission",
+        "transitions": {},
+    },
+    "select_keeper_transaction_submission_b_after_timeout_round": {
+        "name": "Select Keeper Transaction Submission B After Timeout Round",
+        "description": "Selects a new keeper for the transaction submission after a round timeout of the previous keeper",
+        "transitions": {},
+    },
+    "select_keeper_transaction_submission_b_round": {
+        "name": "Select Keeper Transaction Submission B Round",
+        "description": "Selects a new keeper for the transaction submission",
+        "transitions": {},
+    },
+    "service_evicted_round": {
+        "name": "Service Evicted Round",
+        "description": "Terminated the service if it has been evicted from the staking contract",
+        "transitions": {},
+    },
+    "subscription_round": {
+        "name": "Subscription Round",
+        "description": "Purchases a subscription",
+        "transitions": {},
+    },
+    "synchronize_late_messages_round": {
+        "name": "Synchronize Late Messages Round",
+        "description": "Synchronizes any late arriving messages",
+        "transitions": {},
+    },
+    "tool_selection_round": {
+        "name": "Tool Selection Round",
+        "description": "Selects a Mech tool to use to determine the answer of a bet",
+        "transitions": {},
+    },
+    "update_bets_round": {
+        "name": "Update Bets Round",
+        "description": "Fetching the bets and updates them with the latest information",
+        "transitions": {},
+    },
+    "validate_transaction_round": {
+        "name": "Validate Transaction Round",
+        "description": "Validates a transaction",
+        "transitions": {},
+    },
+}
 
 ROUNDS_INFO = {
     "benchmarking_randomness_round": {
