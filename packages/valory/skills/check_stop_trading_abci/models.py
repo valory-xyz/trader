6cefc846158fd378bbd1e45c3f1497e955ef976b
Keshav M <keshav.mishra0298@gmail.com>
2025-05-19 22:50:45 +0530

    Add .dmypy.json to gitignore and gitleaksignore entry

--- packages/valory/skills/check_stop_trading_abci/models.py
+++ packages/valory/skills/check_stop_trading_abci/models.py
@@ -37,6 +37,7 @@ from packages.valory.skills.check_stop_trading_abci.rounds import (
 from packages.valory.skills.mech_interact_abci.models import MechMarketplaceConfig
 from packages.valory.skills.staking_abci.models import StakingParams
 
+
 Requests = BaseRequests
 BenchmarkTool = BaseBenchmarkTool
 
