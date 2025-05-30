7603866f320194c25c28e3f0577ab203e45bd52a
Keshav Mishra <keshav.m882@gmail.com>
2025-05-30 20:16:13 +0530

    :wrench: chore: ununsed import cleaning

--- packages/valory/skills/decision_maker_abci/models.py
+++ packages/valory/skills/decision_maker_abci/models.py
@@ -28,7 +28,6 @@ from pathlib import Path
 from string import Template
 from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Type, Union
 
-from aea.exceptions import enforce
 from aea.skills.base import Model, SkillContext
 from hexbytes import HexBytes
 from web3.constants import HASH_ZERO
@@ -36,21 +35,22 @@ from web3.types import BlockIdentifier
 
 from packages.valory.contracts.multisend.contract import MultiSendOperation
 from packages.valory.skills.abstract_round_abci.base import AbciApp
-from packages.valory.skills.abstract_round_abci.models import ApiSpecs
+from packages.valory.skills.abstract_round_abci.models import ApiSpecs, TypeCheckMixin
 from packages.valory.skills.abstract_round_abci.models import (
     BenchmarkTool as BaseBenchmarkTool,
 )
 from packages.valory.skills.abstract_round_abci.models import Requests as BaseRequests
-from packages.valory.skills.abstract_round_abci.models import TypeCheckMixin
 from packages.valory.skills.decision_maker_abci.policy import EGreedyPolicy
 from packages.valory.skills.decision_maker_abci.redeem_info import Trade
 from packages.valory.skills.decision_maker_abci.rounds import DecisionMakerAbciApp
 from packages.valory.skills.market_manager_abci.bets import Bet
-from packages.valory.skills.market_manager_abci.models import MarketManagerParams
+from packages.valory.skills.market_manager_abci.models import (
+    MarketManagerParams,
+    Subgraph,
+)
 from packages.valory.skills.market_manager_abci.models import (
     SharedState as BaseSharedState,
 )
-from packages.valory.skills.market_manager_abci.models import Subgraph
 from packages.valory.skills.mech_interact_abci.models import (
     Params as MechInteractParams,
 )
