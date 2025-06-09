7cd391b1fbb525e2fd51e2773cf2d9ae6c89a4db
Skanislav <skas.merkushin@gmail.com>
2025-06-09 16:29:24 +0200

    feat: add trader handler for `/agent-info`

--- packages/valory/skills/trader_abci/handlers.py
+++ packages/valory/skills/trader_abci/handlers.py
@@ -20,6 +20,17 @@
 
 """This module contains the handlers for the 'trader_abci' skill."""
 
+import json
+import re
+from typing import Any, Callable, Dict, Generator, List, Optional, Tuple, cast
+from urllib.parse import urlparse
+
+from aea.protocols.base import Message
+
+from packages.valory.connections.http_server.connection import (
+    PUBLIC_ID as HTTP_SERVER_PUBLIC_ID,
+)
+from packages.valory.protocols.http.message import HttpMessage
 from packages.valory.skills.abstract_round_abci.handlers import ABCIRoundHandler
 from packages.valory.skills.abstract_round_abci.handlers import (
     ContractApiHandler as BaseContractApiHandler,
@@ -33,8 +44,13 @@ from packages.valory.skills.abstract_round_abci.handlers import (
 from packages.valory.skills.abstract_round_abci.handlers import (
     TendermintHandler as BaseTendermintHandler,
 )
+from packages.valory.skills.trader_abci.dialogues import (
+    HttpDialogue,
+    HttpDialogues,
+)
 from packages.valory.skills.decision_maker_abci.handlers import (
     HttpHandler as BaseHttpHandler,
+    HttpMethod,
 )
 from packages.valory.skills.decision_maker_abci.handlers import (
     IpfsHandler as BaseIpfsHandler,
@@ -45,10 +61,178 @@ from packages.valory.skills.mech_interact_abci.handlers import (
 
 
 TraderHandler = ABCIRoundHandler
-HttpHandler = BaseHttpHandler
 SigningHandler = BaseSigningHandler
 LedgerApiHandler = BaseLedgerApiHandler
 ContractApiHandler = BaseContractApiHandler
 TendermintHandler = BaseTendermintHandler
 IpfsHandler = BaseIpfsHandler
 AcnHandler = BaseAcnHandler
+
+class HttpHandler(BaseHttpHandler):
+    """This implements the echo handler."""
+
+    SUPPORTED_PROTOCOL = HttpMessage.protocol_id
+
+    def __init__(self, **kwargs: Any) -> None:
+        """Initialize the handler."""
+        super().__init__(**kwargs)
+        self.handler_url_regex: str = ""
+        self.routes: Dict[tuple, list] = {}
+        self.json_content_header: str = ""
+
+    def setup(self) -> None:
+        """Setup the handler."""
+        config_uri_base_hostname = urlparse(
+            self.context.params.service_endpoint
+        ).hostname
+
+        propel_uri_base_hostname = (
+            r"https?:\/\/[a-zA-Z0-9]{16}.agent\.propel\.(staging\.)?autonolas\.tech"
+        )
+
+        local_ip_regex = r"192\.168(\.\d{1,3}){2}"
+
+        # Route regexes
+        hostname_regex = rf".*({config_uri_base_hostname}|{propel_uri_base_hostname}|{local_ip_regex}|localhost|127.0.0.1|0.0.0.0)(:\d+)?"
+        self.handler_url_regex = rf"{hostname_regex}\/.*"
+
+        agent_info_url_regex = rf"{hostname_regex}\/agent-info"
+
+        self.routes = {
+            (HttpMethod.GET.value, HttpMethod.HEAD.value): [
+                (agent_info_url_regex, self._handle_get_agent_info),
+            ],
+        }
+
+        self.json_content_header = "Content-Type: application/json\n"
+
+
+    def _handle_get_agent_info(
+        self, http_msg: HttpMessage, http_dialogue: HttpDialogue
+    ) -> None:
+        """Handle a Http request of verb GET."""
+        data = {
+            "address": self.context.agent_address,
+        }
+        self.context.logger.info(f"Sending agent info: {data=}")
+        self._send_ok_response(http_msg, http_dialogue, data)
+
+    def _send_ok_response(
+        self, http_msg: HttpMessage, http_dialogue: HttpDialogue, data: Dict
+    ) -> None:
+        """Send an OK response with the provided data"""
+        http_response = http_dialogue.reply(
+            performative=HttpMessage.Performative.RESPONSE,
+            target_message=http_msg,
+            version=http_msg.version,
+            status_code=200,
+            status_text="Success",
+            headers=f"{self.json_content_header}{http_msg.headers}",
+            body=json.dumps(data).encode("utf-8"),
+        )
+
+        # Send response
+        self.context.logger.info("Responding with: {}".format(http_response))
+        self.context.outbox.put_message(message=http_response)
+
+    def _handle_bad_request(
+        self, http_msg: HttpMessage, http_dialogue: HttpDialogue
+    ) -> None:
+        """
+        Handle a Http bad request.
+
+        :param http_msg: the http message
+        :param http_dialogue: the http dialogue
+        """
+        http_response = http_dialogue.reply(
+            performative=HttpMessage.Performative.RESPONSE,
+            target_message=http_msg,
+            version=http_msg.version,
+            status_code=400,
+            status_text="Bad request",
+            headers=http_msg.headers,
+            body=b"",
+        )
+
+        # Send response
+        self.context.logger.info("Responding with: {}".format(http_response))
+        self.context.outbox.put_message(message=http_response)
+
+    def _get_handler(self, url: str, method: str) -> Tuple[Optional[Callable], Dict]:
+        """Check if an url is meant to be handled in this handler
+
+        We expect url to match the pattern {hostname}/.*,
+        where hostname is allowed to be localhost, 127.0.0.1 or the service_endpoint's hostname.
+
+        :param url: the url to check
+        :param method: the method
+        :returns: the handling method if the message is intended to be handled by this handler, None otherwise, and the regex captures
+        """
+        # Check base url
+        if not re.match(self.handler_url_regex, url):
+            self.context.logger.info(
+                f"The url {url} does not match the HttpHandler's pattern"
+            )
+            return None, {}
+
+        # Check if there is a route for this request
+        for methods, routes in self.routes.items():
+            if method not in methods:
+                continue
+
+            for route in routes:
+                # Routes are tuples like (route_regex, handle_method)
+                m = re.match(route[0], url)
+                if m:
+                    return route[1], m.groupdict()
+
+        # No route found
+        self.context.logger.info(
+            f"The message [{method}] {url} is intended for the HttpHandler but did not match any valid pattern"
+        )
+        return self._handle_bad_request, {}
+
+    def handle(self, message: Message) -> None:
+        """
+        Implement the reaction to an envelope.
+
+        :param message: the message
+        """
+        http_msg = cast(HttpMessage, message)
+
+        # Check if this is a request sent from the http_server skill
+        if (
+            http_msg.performative != HttpMessage.Performative.REQUEST
+            or message.sender != str(HTTP_SERVER_PUBLIC_ID.without_hash())
+        ):
+            super().handle(message)
+            return
+
+        # Check if this message is for this skill. If not, send to super()
+        handler, kwargs = self._get_handler(http_msg.url, http_msg.method)
+        if not handler:
+            super().handle(message)
+            return
+
+        # Retrieve dialogues
+        http_dialogues = cast(HttpDialogues, self.context.http_dialogues)
+        http_dialogue = cast(HttpDialogue, http_dialogues.update(http_msg))
+
+        # Invalid message
+        if http_dialogue is None:
+            self.context.logger.info(
+                "Received invalid http message={}, unidentified dialogue.".format(
+                    http_msg
+                )
+            )
+            return
+
+        # Handle message
+        self.context.logger.info(
+            "Received http request with method={}, url={} and body={!r}".format(
+                http_msg.method,
+                http_msg.url,
+                http_msg.body,
+            )
+        )
+        handler(http_msg, http_dialogue, **kwargs)
