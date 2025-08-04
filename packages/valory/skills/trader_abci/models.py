7ca023ce7ec3f997bfefc6e33d36f5175fcf3e82
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-08-05 00:28:06 +0530

    fix: event timeout

--- packages/valory/skills/trader_abci/models.py
+++ packages/valory/skills/trader_abci/models.py
@@ -26,6 +26,7 @@ from packages.valory.skills.abstract_round_abci.models import (
     BenchmarkTool as BaseBenchmarkTool,
 )
 from packages.valory.skills.abstract_round_abci.models import Requests as BaseRequests
+from packages.valory.skills.chatui_abci.rounds import Event as ChatuiEvent
 from packages.valory.skills.check_stop_trading_abci.models import CheckStopTradingParams
 from packages.valory.skills.decision_maker_abci.models import (
     AccuracyInfoFields as BaseAccuracyInfoFields,
@@ -152,14 +153,15 @@ class SharedState(BaseSharedState):
             DecisionMakerEvent,
             TSEvent,
             ResetPauseEvent,
+            ChatuiEvent,
         )
         round_timeout = params.round_timeout_seconds
         round_timeout_overrides = {
             cast(EventType, event).ROUND_TIMEOUT: round_timeout for event in events
         }
-        round_timeout_overrides[
-            MechInteractEvent.ROUND_TIMEOUT
-        ] = params.mech_interact_round_timeout_seconds
+        round_timeout_overrides[MechInteractEvent.ROUND_TIMEOUT] = (
+            params.mech_interact_round_timeout_seconds
+        )
         reset_pause_timeout = params.reset_pause_duration + MARGIN
         event_to_timeout_overrides: EventToTimeoutMappingType = {
             **round_timeout_overrides,
