c613683ead5285896a416deaaffb0f9320499ccd
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-08-11 19:59:48 +0530

    chore: added comment

--- packages/valory/skills/decision_maker_abci/behaviours/base.py
+++ packages/valory/skills/decision_maker_abci/behaviours/base.py
@@ -467,6 +467,7 @@ class DecisionMakerBaseBehaviour(BetsManagerBehaviour, ABC):
         yield from self.download_strategies()
         yield from self.wait_for_condition_with_sleep(self.check_balance)
 
+        # accessing `self.shared_state.chatui_config` calls `self._ensure_chatui_store()` which ensures `trading_strategy` can never be `None`
         next_strategy: str = self.shared_state.chatui_config.trading_strategy  # type: ignore[assignment]
 
         tried_strategies: Set[str] = set()
