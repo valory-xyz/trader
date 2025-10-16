1009cc54853361fc4cd970cbc649b020fd36971a
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-10-17 01:12:21 +0530

    fix: copyright header

--- packages/valory/skills/market_manager_abci/graph_tooling/requests.py
+++ packages/valory/skills/market_manager_abci/graph_tooling/requests.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 #
-#   Copyright 2021-2024 Valory AG
+#   Copyright 2021-2025 Valory AG
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -88,9 +88,9 @@ class QueryingBehaviour(BaseBehaviour, ABC):
         super().__init__(**kwargs)
         self._call_failed: bool = False
         self._fetch_status: FetchStatus = FetchStatus.NONE
-        self._creators_iterator: Iterator[
-            Tuple[str, List[str]]
-        ] = self.params.creators_iterator
+        self._creators_iterator: Iterator[Tuple[str, List[str]]] = (
+            self.params.creators_iterator
+        )
         self._current_market: str = ""
         self._current_creators: List[str] = []
 
