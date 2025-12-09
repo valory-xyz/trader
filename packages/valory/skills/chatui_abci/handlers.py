25a5fd8ba8f25113dddaa9bd05538e6dc5059e34
DivyaNautiyal07 <divyanautiyal07@gmail.com>
2025-12-10 01:20:09 +0530

    fix: inherit from agent performance http handler

--- packages/valory/skills/chatui_abci/handlers.py
+++ packages/valory/skills/chatui_abci/handlers.py
@@ -44,7 +44,7 @@ from packages.valory.skills.abstract_round_abci.handlers import AbstractResponse
 from packages.valory.skills.abstract_round_abci.handlers import (
     ContractApiHandler as BaseContractApiHandler,
 )
-from packages.valory.skills.abstract_round_abci.handlers import (
+from packages.valory.skills.agent_performance_summary_abci.handlers import (
     HttpHandler as BaseHttpHandler,
 )
 from packages.valory.skills.abstract_round_abci.handlers import (
